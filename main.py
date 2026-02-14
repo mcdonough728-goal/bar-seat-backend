751dad1d9a67d8863b9c70756fb47044fcc223d9
mcdonough728-goal <mcdonough728@gmail.com>
2026-02-14 15:22:00 -0500

    Allow Werkzeug in production for Render

--- main.py
+++ main.py
@@ -118,6 +118,7 @@ def last_update(place_id):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
+
 
    
